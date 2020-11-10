2276a7dcb29ede64e77e40dcfbf6201ecaccecb9
Yangwook Jeong <ywjeong@firecop365.com>
2020-11-10 09:46:17 +0900

    [add] splash screen library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {ApolloProvider} from '@apollo/react-hooks'
 import {RecoilRoot} from 'recoil'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
+import SplashScreen from 'react-native-splash-screen'
 
 import AppContainer from 'src/navigators'
 import client from 'src/services'
 
 const App = () => {
+	useEffect(() => SplashScreen.hide(), [])
+
 	return (
 		<SafeAreaProvider>
 			<ApolloProvider client={client}>
